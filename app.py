9cabca8e55b364ccf4920678c22349ee658ed3f3
Alexander Pasynkov <a.pasynkov@napoleonit.ru>
2019-03-17 23:14:07 +0500

    first commit

--- app.py
+++ app.py
@@ -4,23 +4,21 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config['JSON_SORT_KEYS'] = False
 
-
-
 taskGet = [
-  {
-    "id": "10dcdccb-8876-4245-ac53-92900c6509bd",
-    "title": "Первая заметка",
-    "text": "Вторая заметка",
-    "date_create": 1513759529,
-    "date_update": 1517259529
-  },
-  {
-    "id": "10dcdccb-8876-4245-ac53-92900c6509bd",
-    "title": "Первая заметка",
-    "text": "Вторая заметка",
-    "date_create": 1513759529,
-    "date_update": 1517259529
-  }
+    {
+        "id": "10dcdccb-8876-4245-ac53-92900c6509bd",
+        "title": "Первая заметка",
+        "text": "Вторая заметка",
+        "date_create": 1513759529,
+        "date_update": 1517259529
+    },
+    {
+        "id": "10dcdccb-8876-4245-ac53-92900c6509bd",
+        "title": "Первая заметка",
+        "text": "Вторая заметка",
+        "date_create": 1513759529,
+        "date_update": 1517259529
+    }
 ]
 
 taskT = [
@@ -30,13 +28,14 @@ taskT = [
 ]
 
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def GET():
     if len(taskGet) == 0:
         abort(404)
     return jsonify(taskGet)
 
-@app.route('/', methods = ['POST'])
+
+@app.route('/', methods=['POST'])
 def POST():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -49,26 +48,30 @@ def POST():
     ]
     return jsonify(task), 201
 
-@app.route('/', methods = ['PUT'])
+
+@app.route('/', methods=['PUT'])
 def PUT():
     if not request.json or not 'id' in request.json:
         abort(400)
     return jsonify(taskT)
 
-@app.route('/', methods = ['DELETE'])
+
+@app.route('/', methods=['DELETE'])
 def DELETE():
     if not request.json or not 'id' in request.json:
         abort(400)
     return jsonify(taskT)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=8080)
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
